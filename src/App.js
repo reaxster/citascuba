e5815c9fad66463df3649df1ed8c340d94658172
unknown <Richar@Richar-Personal.cariberoyale.com>
2021-02-09 11:01:00 -0500

    Not chuncks

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import Loading from "./components/Loading";
 import Footer from "./components/Footer";
-//import FormPage from "./pages/FormPage";
-//import TablePage from "./pages/TablePage";
-//import InterviewSummary from "./pages/InterviewSummary";
-///import DisclosurePage from "./pages/DisclosurePage";
-//import News from "./pages/News";
+import FormPage from "./pages/FormPage";
+import TablePage from "./pages/TablePage";
+import InterviewSummary from "./pages/InterviewSummary";
+import DisclosurePage from "./pages/DisclosurePage";
+import News from "./pages/News";
 
-const FormPage = React.lazy(() => import("./pages/FormPage"));
+/*const FormPage = React.lazy(() => import("./pages/FormPage"));
 const TablePage = React.lazy(() => import("./pages/TablePage"));
 const InterviewSummary = React.lazy(() => import("./pages/InterviewSummary"));
 const DisclosurePage = React.lazy(() => import("./pages/DisclosurePage"));
-const News = React.lazy(() => import("./pages/News"));
+const News = React.lazy(() => import("./pages/News"));*/
 
 function App() {
   return (
